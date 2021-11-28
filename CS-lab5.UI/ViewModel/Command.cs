7959d066a80fbe20eaaf5bfa7fc310fafcae2c62
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-11-29 01:36:47 +0200

    Add commands

---
+++ CS-lab5.UI/ViewModel/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CS_lab5.UI.ViewModel {
+    public class Command : ICommand {
+        public Command(Action<object> action) {
+            ExecuteDelegate = action;
+        }
+
+        public Command(Action<object> action, Predicate<object> isExecutable) {
+            ExecuteDelegate = action;
+            CanExecuteDelegate = isExecutable;
+        }
+
+        public Predicate<object> CanExecuteDelegate { get; set; }
+        public Action<object> ExecuteDelegate { get; set; }
+
+        public bool CanExecute(object parameter) {
+            return CanExecuteDelegate?.Invoke(parameter) ?? true;
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter) {
+            ExecuteDelegate?.Invoke(parameter);
+        }
+    }
+}
--- CS-lab5.UI/ViewModel/DataViewModel.cs
+++ CS-lab5.UI/ViewModel/DataViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CS_lab5.UI.ViewModel {
     class DataViewModel : BaseViewModel {
@@ -16,6 +17,7 @@ namespace CS_lab5.UI.ViewModel {
                 OnPropertyChanged(nameof(Clients));
             }
         }
+
         private ObservableCollection<Doctor> __doctors;
         public ObservableCollection<Doctor> Doctors {
             get { return __doctors; } 
@@ -24,6 +26,7 @@ namespace CS_lab5.UI.ViewModel {
                 OnPropertyChanged(nameof(Doctors));
             }
         }
+        
         private ObservableCollection<PatientData> __patientData;
         public ObservableCollection<PatientData> PatientData {
             get { return __patientData; } 
@@ -32,6 +35,7 @@ namespace CS_lab5.UI.ViewModel {
                 OnPropertyChanged(nameof(PatientData));
             }
         }
+        
         private ObservableCollection<AnalisisResult> __analisisResults;
         public ObservableCollection<AnalisisResult> AnalisisResults {
             get { return __analisisResults; } 
@@ -41,11 +45,78 @@ namespace CS_lab5.UI.ViewModel {
             }
         }
 
+        private ObservableCollection<User> __users;
+        public ObservableCollection<User> Users {
+            get { return __users; }
+        }
+
+        private User __selectedUser;
+        public User SelectedUser {
+            get { return __selectedUser; }
+            set { 
+                __selectedUser = value; 
+                OnPropertyChanged(nameof(SelectedUser));
+            }
+        }
+
+        private Client __selectedClient;
+        public Client SelectedClient {
+            get { return __selectedClient; }
+            set {
+                __selectedClient = value;
+                OnPropertyChanged(nameof(SelectedClient));
+            }
+        }
+
+        private string __appMode = "Clients";
+        public string AppMode {
+            get { return __appMode; }
+            set {
+                __appMode = value;
+                OnPropertyChanged(nameof(AppMode));
+            }
+        }
+
+        public ICommand SetAppMode { get; set; }
+        public void SetAppModeCmd(object args) {
+            AppMode = args.ToString();
+        }
+        public bool CanSwitchAppMode(object args) {
+            return AppMode != args.ToString();
+        }
+
+        public ICommand DeleteSelectedUser { get; set; }
+        public void DeleteSelectedUserCmd(object args) {
+            if(SelectedUser.GetType() == typeof(Client)) {
+                Clients.Remove((Client)SelectedUser);
+            } else if(SelectedUser.GetType() == typeof(Doctor)) {
+                Doctors.Remove((Doctor)SelectedUser);
+            } else {
+                throw new ArgumentException("Wrong type of selected user");
+            }
+            SelectedUser = null;
+            __users = getAllUsers();
+            OnPropertyChanged(nameof(Users));
+        }
+        public bool CanDeleteSelectedUser(object args) {
+            return SelectedUser != null;
+        }
+
         public DataViewModel(DataModel model) {
             __clients = new ObservableCollection<Client>(model.Clients);
             __doctors = new ObservableCollection<Doctor>(model.Doctors);
             __patientData = new ObservableCollection<PatientData>(model.PatientData);
             __analisisResults = new ObservableCollection<AnalisisResult>(model.AnalisisResults);
+
+            __users = new ObservableCollection<User>(((IEnumerable<User>)model.Doctors).Concat((IEnumerable<User>)model.Clients));
+
+            SetAppMode = new Command(SetAppModeCmd, CanSwitchAppMode);
+            DeleteSelectedUser = new Command(DeleteSelectedUserCmd, CanDeleteSelectedUser);
+        }
+
+        private ObservableCollection<User> getAllUsers() {
+            var users = ((IEnumerable<User>)Doctors).Concat((IEnumerable<User>)Clients);
+            return new ObservableCollection<User>(users);
         }
 
         public DataModel ToDataModel() {
