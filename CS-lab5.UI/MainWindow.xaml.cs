7a122d31cd9d182c6f7edb3a435fa03c5217daf5
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-11-28 15:52:05 +0200

    resolve mc

--- CS-lab5.UI/MainWindow.xaml.cs
+++ CS-lab5.UI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CS_lab5.UI.Notifications;
 
 namespace CS_lab5.UI
 {
