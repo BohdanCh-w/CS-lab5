b79d412c738b0d5b7c57e7a02dd06ffb106792e7
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-11-28 15:53:39 +0200

    Add Notification - Convertor

---
+++ CS-lab5.UI/Notifications/Notification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_lab5.UI.Notifications {
+    public enum NotificationType {
+        INFO,
+        WARNING,
+        ERROR,
+        __UnexpectedType
+    }
+
+    public class Notification {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public NotificationType Type { get; set; }
+
+        public Notification(string title, string message, NotificationType type) {
+            Title = title;
+            Message = message;
+            Type = type;
+        }    
+    }
+}
